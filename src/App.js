e9c2a40012d99b342518f8f0f534cbe8cbb290af
Ajani Myers <ajanimyersthedev@gmail.com>
2020-11-24 14:54:01 -0700

    Created random recipe generator front-end, and connected to spoonacular

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./assets/css/typography.css";
+import "./assets/css/main.css";
+import parse from "html-react-parser";
+require("dotenv").config();
 function App() {
+  const url = `https://api.spoonacular.com/recipes/random?number=1&apiKey=${process.env.REACT_APP_API_KEY}`;
+  const [recipe, setRecipe] = useState({});
+  const getRecipe = () => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((resp) => {
+        setRecipe(resp.recipes[0]);
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>
+        Random Recipe Generator. <span className="img">🍳</span>
+      </h1>
+      <button onClick={getRecipe}>Generate Random Recipe</button>
+      {Object.keys(recipe).length > 0 ? (
+        <div>
+          <h3>{recipe.title}</h3>
+          <p dangerouslySetInnerHTML={{ __html: recipe.summary }}></p>
+          <div className="flex-container">
+            <div>
+              <h6>Ingredients</h6>
+              <ul>
+                {recipe.extendedIngredients.map((ingredient) => {
+                  return (
+                    <li>
+                      {ingredient.name} ({ingredient.original})
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+            <div>
+              <h6>Instructions</h6>
+              {parse(recipe.instructions)}
+            </div>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 }
